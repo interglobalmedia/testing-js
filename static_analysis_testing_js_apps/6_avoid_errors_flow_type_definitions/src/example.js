17c06fa9f222b3a03987426e730e8207df512301
interglobalmedia <interglobalmedia@gmail.com>
2019-07-26 14:52:34 -0400

    Add 2/6 fix vulnerabilities

---
+++ static_analysis_testing_js_apps/6_avoid_errors_flow_type_definitions/src/example.js
@@ -0,0 +1,12 @@
+const name = 'Freddy'
+typeof name === 'string'
+
+if (!('serviceWorker' in navigator)) {
+  // you have an old browser :-(
+}
+
+const greeting = 'hello'
+console.log(`${greeting} world!`)
+;[(1, 2, 3)].forEach(x => console.log(x))
+
+// prettier caused problems in this configuration and validation did NOT pass
