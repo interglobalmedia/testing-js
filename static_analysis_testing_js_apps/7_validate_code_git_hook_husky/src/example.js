a9bc6a7c1fe9c1410dd9b02e3719ca677a338d3f
interglobalmedia <interglobalmedia@gmail.com>
2019-07-26 14:53:57 -0400

    Add 2/7 fix vulnerabilities

---
+++ static_analysis_testing_js_apps/7_validate_code_git_hook_husky/src/example.js
@@ -0,0 +1,9 @@
+const name = 'Freddy'
+typeof name === 'string'
+
+if (!('serviceWorker' in navigator)) {
+  // you have an old browser :-(
+}
+
+const greeting = 'hello'
+console.log(`${greeting} world!`)
