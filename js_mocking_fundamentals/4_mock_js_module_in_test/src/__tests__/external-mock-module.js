5897c17bdcfc5646484cb5f209d0023b68b007ee
interglobalmedia <interglobalmedia@gmail.com>
2019-07-26 15:12:15 -0400

    Add 3/4 fix vulnerabilities

---
+++ js_mocking_fundamentals/4_mock_js_module_in_test/src/__tests__/external-mock-module.js
@@ -0,0 +1,17 @@
+const thumbWar = require('../thumb-war')
+const utilsMock = require('../utils')
+console.log(require.cache)
+
+jest.mock('../utils')
+
+test('returns winner', () => {
+    const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
+    expect(winner).toBe('Kent C. Dodds')
+    expect(utilsMock.getWinner.mock.calls).toEqual([
+        ['Kent C. Dodds', 'Ken Wheeler'],
+        ['Kent C. Dodds', 'Ken Wheeler']
+    ])
+
+    // cleanup
+    utilsMock.getWinner.mockReset()
+})
