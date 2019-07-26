2de5e849e4266d4375dfa582b20e975d32e8a67a
interglobalmedia <interglobalmedia@gmail.com>
2019-07-26 15:25:32 -0400

    Add 4/3 fix vulnerabilities

---
+++ configure_jest_for_testing_js_apps/3_config_jest_test_env__node_or_browser_code/src/shared/utils.js
@@ -0,0 +1,17 @@
+function getFormattedValue(value, language = 'en-US') {
+    let formattedValue = parseFloat(value).toLocaleString(language, {
+        useGrouping: true,
+        maximumFractionDigits: 6,
+    })
+
+    // console.log(window)
+    // Add back missing .0 in e.g. 12.0
+    const match = value.match(/\.\d*?(0*)$/)
+
+    if (match) {
+        formattedValue += /[1-9]/.test(match[0]) ? match[1] : match[0]
+    }
+    return formattedValue
+}
+
+export { getFormattedValue }
