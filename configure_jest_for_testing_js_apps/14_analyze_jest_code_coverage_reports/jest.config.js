006179d90c4d8b5cc800b07b3dee3bfd380b77fd
interglobalmedia <interglobalmedia@gmail.com>
2019-07-26 15:45:21 -0400

    Add 4/14

---
+++ configure_jest_for_testing_js_apps/14_analyze_jest_code_coverage_reports/jest.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+module.exports = {
+    testEnvironment: 'jest-environment-jsdom',
+    moduleDirectories: [
+        'node_modules',
+        path.join(__dirname, 'src'),
+        'shared',
+        path.join(__dirname, 'test'),
+    ],
+    moduleNameMapper: {
+        '\\.module\\.css$': 'identity-obj-proxy',
+        '\\.css$': require.resolve('./test/style-mock.js'),
+    },
+    setupFilesAfterEnv: ['<rootDir>/test/setup-tests.js'],
+    collectCoverageFrom: ['**/src/**/*.js'],
+}
