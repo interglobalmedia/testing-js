e72c3a0701a10778fe500914663a52481f8e2e08
interglobalmedia <interglobalmedia@gmail.com>
2019-07-27 17:37:28 -0400

    Add 4/22 4/23 4/24

---
+++ configure_jest_for_testing_js_apps/23_run_eslint_with_jest_using_jest_runner_eslint/webpack.config.js
@@ -0,0 +1,45 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        modules: ['node_modules', path.join(__dirname, 'src'), 'shared'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /\.module\.css$/,
+                use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+            },
+            {
+                test: /\.module\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader', options: { modules: true, camelCase: true } },
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                use: 'file-loader',
+            },
+        ],
+    },
+}
+
+if (process.env.WEBPACK_SERVE) {
+    module.exports.mode = 'development'
+    module.exports.serve = {
+        content: path.join(__dirname, './public'),
+    }
+}
